628568eab2e8a2d4e4a68e7dfddfdb4b2d964fbf
João Godoi <joaogodoi.dev@gmail.com>
2022-11-23 20:31:36 -0300

    :sparkles: Created logic to find all people (mocked)

--- src/main/java/com/joaogodoi/SpringBoot3/services/PersonService.java
+++ src/main/java/com/joaogodoi/SpringBoot3/services/PersonService.java
@@ -3,6 +3,8 @@ package com.joaogodoi.SpringBoot3.services;
 import com.joaogodoi.SpringBoot3.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -18,6 +20,26 @@ public class PersonService {
         person.setId(counter.getAndIncrement());
         person.setFirstName("João");
         person.setLastName("Godoi");
+        person.setAddress("São Paulo - Brazil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> personList = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            personList.add(person);
+        }
+        logger.info("Finding all people...");
+        return personList;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.getAndIncrement());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
         person.setAddress("São Paulo - Brazil");
         person.setGender("Male");
         return person;
